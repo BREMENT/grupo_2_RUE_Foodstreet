f9c03ad46989687d36884bda804d99e33e192d35
FlavioValladolid <flavioisay@hotmail.com>
2021-06-10 14:09:12 -0600

    change app.js

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + '/views/home.html');
 });
 
+app.get('/home', (req,res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/producto', (req,res) => {
+    res.sendFile(__dirname + '/views/detalleProducto.html');
+});
+
+app.get('/inicio', (req,res) => {
+    res.sendFile(__dirname + '/views/inicio.html');
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/registro', (req,res) => {
+    res.sendFile(__dirname + '/views/registro.html');
+});
+
 app.listen(3000, () => {
     console.log('Servidor funcionando');
 });
