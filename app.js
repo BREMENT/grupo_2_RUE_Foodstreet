99fe1d7590c15df904f36bc5aa8fbda6110ee841
luis castillo <lc77278@gmail.com>
2021-07-14 20:25:16 -0500

    mvc structura

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 const express = require('express');
 const app = express();
+const userRouter = require('./routes/user.js');
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
+app.use('/user', userRouter);
+app.get('/', (req, res)=>{
+    res.render('inicio');
+})
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/inicio.html');
-});
+// app.get('/restaurante', (req, res) => {
+//     res.sendFile(__dirname + '/views/detalleRestaurante.html');
+// });
 
-app.get('/home', (req, res) => {
-    res.sendFile(__dirname + '/views/inicio.html');
-});
+// app.get('/producto', (req, res) => {
+//     res.sendFile(__dirname + '/views/detalleProducto.html');
+// });
 
-app.get('/inicio', (req, res) => {
-    res.sendFile(__dirname + '/views/inicio.html');
-});
+// app.get('/login', (req, res) => {
+//     res.sendFile(__dirname + '/views/login.html');
+// });
 
-app.get('/restaurante', (req, res) => {
-    res.sendFile(__dirname + '/views/detalleRestaurante.html');
-});
-
-app.get('/producto', (req, res) => {
-    res.sendFile(__dirname + '/views/detalleProducto.html');
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/registro', (req, res) => {
-    res.sendFile(__dirname + '/views/registro.html');
-});
+// app.get('/registro', (req, res) => {
+//     res.sendFile(__dirname + '/views/registro.html');
+// });
 
 app.listen(3000, () => {
     console.log('Servidor funcionando');
