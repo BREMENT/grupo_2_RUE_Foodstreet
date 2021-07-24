498a1a6ff4c66a83dcfc1d79e58974038ceb3d5a
Luis Castillo <lc77278@gmail.com>
2021-07-23 20:25:23 -0500

    routes y controller form product

--- controllers/producto.js
+++ controllers/producto.js
@@ -1,7 +1,20 @@
 const productoController = {
     detalle: (req, res)=>{
         res.render('detalleProducto');
+    },
+    create: (req, res)=>{
+        res.send('formulario-creacion');
+    },
+    store: (req, res)=>{
+        res.send('datos del formulario');
+    },
+    edit: (req, res)=>{
+        res.send('formulario-editar');
+    },
+    update: (req, res)=>{
+        res.send('datos del formulario');
     }
+
 };
 
 module.exports = productoController;
--- routes/producto.js
+++ routes/producto.js
@@ -4,5 +4,13 @@ const productoController = require('../controllers/producto.js');
 
 router.get('/producto', productoController.detalle);
 
+// formulario para crear producto
+router.get('/crear', productoController.create);
+router.post('/crear', productoController.store);
+
+// formulario para editar producto
+router.get('/:id/editar', productoController.edit);
+router.put('/:id/editar', productoController.update);
+
 
 module.exports = router;
