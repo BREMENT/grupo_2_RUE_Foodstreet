1992fd7e15704de3240abcc23ed5454e2056317d
aura-cyber <auramatahdz@outlook.com>
2021-08-13 00:14:48 -0500

    lectura user JSON

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+
+const pathFile = path.join(__dirname, '../src/data/user.json');
+const pathJson = JSON.parse(fs.readFileSync(pathFile, 'utf-8'));
+
 const userController = {
-    login: (req, res)=>{
+    login: (req, res) => {
         res.render('users/login');
     },
-    signup: (req, res)=>{
+    signup: (req, res) => {
         res.render('users/registro');
+
+        console.log(pathJson);
+
     },
-    create: (req, res)=>{
+    create: (req, res) => {
         // registrar usuario
         res.send('registrar usuario');
     }
