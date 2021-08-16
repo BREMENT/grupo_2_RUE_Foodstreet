3ea4a1b261d9fa8d29a693a16a29001eebe6ff2d
Brenda Mendez <mendez.brenda2013@gmail.com>
2021-08-16 15:25:20 -0400

    Se genera forEach para cargar menu en json

--- controllers/producto.js
+++ controllers/producto.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../src/data/menu.json');   
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const productoController = {
     detalle: (req, res)=>{
         res.render('detalleProducto');
     },
     products: (req, res)=>{
-        res.render('products');
+        const visited = products.filter(product => product.category === 'visited')
+        const inSale = products.filter(product => product.category === 'in-sale')
+        console.log({visited, inSale})
+        res.render('products', {visited: visited, inSale:inSale})
     },
     create: (req, res)=>{
         res.render('product-create-form');
