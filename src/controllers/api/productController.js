65cc13c25876aba5ce7ba7e14c8c757294ac12ee
Luis Castillo <lc77278@gmail.com>
2021-10-22 16:18:24 -0500

    product paginatio

--- src/controllers/api/productController.js
+++ src/controllers/api/productController.js
@@ -1,80 +1,107 @@
-const {request, response} = require('express');
-const db = require('../../database/models');
+const { request, response } = require("express");
+const db = require("../../database/models");
 
 const productController = {
-    getProducts: (req = request, res = response)=>{
+  getProducts: async (req = request, res = response) => {
+    try {
+      const pagina = Number(req.query.page) || 1;
+      const paginado = pagina > 0 ? (pagina - 1) * 10 : 1; 
+      let respuesta = {};
+      
+      const [totalProduct, products] = await Promise.all([
+        db.Producto.count({ where: { estatus: 1 } }),
         db.Producto.findAll({
-            where:{
-                estatus: 1
-            },
-            attributes:[
-                ['producto_id', 'id'],
-                ['nombre','name'],
-                ['descripcion', 'description'],
-                [
-                    db.Sequelize.fn('concat', '/api/products/',db.Sequelize.col('producto_id')),
-                    'detail'
-                ]
+          where: {
+            estatus: 1,
+          },
+          limit: 10,
+          offset: paginado,
+          attributes: [
+            ["producto_id", "id"],
+            ["nombre", "name"],
+            ["descripcion", "description"],
+            [
+              db.Sequelize.fn(
+                "concat",
+                "/api/products/",
+                db.Sequelize.col("producto_id")
+              ),
+              "detail",
             ],
-            include:[
-                {association: 'TipoComida'},
-                {association: 'TipoCategoria'}
-            ]
-        })
-        .then(data =>{
-            console.log(data);
-            res.status(200).json({
-                meta:{
-                    status: 200,
-                    url: '/api/products',
-                    total: data.length
-                },
-                products: data
-            });
-        })
-        .catch(console.log);
+          ],
+          include: [
+            { association: "TipoComida" },
+            { association: "TipoCategoria" },
+          ],
+        }),
+      ]);
+      if (products.length >= 1 && pagina >= 0) {
+        respuesta = {
+          meta: {
+            status: 200,
+            url: "/api/products",
+            total_products: totalProduct,
+            total_page: products.length,
+            next:
+              products.length === 10 ? `/api/products/?page=${pagina + 1}` : "",
+            previous: pagina > 1 ? `/api/products/?page=${pagina - 1}` : ``,
+          },
+          products,
+        };
 
-    },
-    getProduct: (req = request, res = response)=>{
-        const id = Number(req.params.id);
-        db.Producto.findOne({
-            where:{
-                estatus: 1,
-                producto_id: id
+      }else{
+        respuesta = {
+            meta:{
+                status: 200,
+                url: "/api/products",
             },
-            attributes:[
-                ['producto_id', 'id'],
-                ['nombre', 'name'],
-                ['descripcion', 'description'],
-                ['precio', 'price'],
-                ['descuento', 'discount'],
-                [
-                    db.Sequelize.fn(
-                        'concat',
-                        '/images/products/',
-                        db.Sequelize.col('foto')
-                    ),
-                    'img'
-                ]
-            ],
-            include:[
-                {association:'TipoComida'},
-                {association:'TipoCategoria'}
-            ]
-        })
-        .then( data =>{
-            res.status(200).json({
-                meta:{
-                    status: 200,
-                    total: 1,
-                    url: `/api/products/${id}`
-                },
-                products: data
-            });
-        })
-        .catch(console.log);
+            msg: `Ya no hay productos en la pagina ${pagina} que busca`
+        }
+      }
+      res.status(200).json(respuesta);
+    } catch (err) {
+      console.log(err);
     }
-
-}
+  },
+  getProduct: (req = request, res = response) => {
+    const id = Number(req.params.id);
+    db.Producto.findOne({
+      where: {
+        estatus: 1,
+        producto_id: id,
+      },
+      attributes: [
+        ["producto_id", "id"],
+        ["nombre", "name"],
+        ["descripcion", "description"],
+        ["precio", "price"],
+        ["descuento", "discount"],
+        [
+          db.Sequelize.fn(
+            "concat",
+            "/images/products/",
+            db.Sequelize.col("foto")
+          ),
+          "img",
+        ],
+      ],
+      include: [
+        { association: "TipoComida" },
+        { association: "TipoCategoria" },
+      ],
+    })
+      .then((data) => {
+        res.status(200).json({
+          meta: {
+            status: 200,
+            total: 1,
+            url: `/api/products/${id}`,
+          },
+          products: data,
+        });
+      })
+      .catch(console.log);
+  },
+};
 
 module.exports = productController;
