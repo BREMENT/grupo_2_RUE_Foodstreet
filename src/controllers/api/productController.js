ba2af517652bff1b195b67147af0c83045c31ca5
Luis Castillo <lc77278@gmail.com>
2021-10-21 23:04:39 -0500

    endpont products id

--- src/controllers/api/productController.js
+++ src/controllers/api/productController.js
@@ -36,7 +36,43 @@ const productController = {
 
     },
     getProduct: (req = request, res = response)=>{
-        res.status(200).json({msg: 'ready'});
+        const id = Number(req.params.id);
+        db.Producto.findOne({
+            where:{
+                estatus: 1,
+                producto_id: id
+            },
+            attributes:[
+                ['producto_id', 'id'],
+                ['nombre', 'name'],
+                ['descripcion', 'description'],
+                ['precio', 'price'],
+                ['descuento', 'discount'],
+                [
+                    db.Sequelize.fn(
+                        'concat',
+                        '/images/products/',
+                        db.Sequelize.col('foto')
+                    ),
+                    'img'
+                ]
+            ],
+            include:[
+                {association:'TipoComida'},
+                {association:'TipoCategoria'}
+            ]
+        })
+        .then( data =>{
+            res.status(200).json({
+                meta:{
+                    status: 200,
+                    total: 1,
+                    url: `/api/products/${id}`
+                },
+                products: data
+            });
+        })
+        .catch(console.log);
     }
 
 }
