880249a55a4055604562dd9f16a3d80d408c4faa
Luis Castillo <lc77278@gmail.com>
2021-10-23 21:04:45 -0500

    modificando home.ejs

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,25 @@
+const {request, response} = require('express');
+const db = require('../database/models');
+
 const indexController = {
-    index: (req, res)=>{
-        res.render('inicio');
+    index: async(req = request, res = response)=>{
+
+        try {
+            const products = await db.Producto.findAll({
+                where: {
+                    estatus: 1
+                },
+                order:[
+                    ['producto_id','desc']
+                ],
+                limit: 4
+            });
+            // console.log(products);
+            res.render('inicio', {products});
+        } catch (err) {
+            console.log(err);
+        }
+
     }
 }
 
